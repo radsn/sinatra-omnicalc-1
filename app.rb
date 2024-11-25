ab4983407b0b054c9a2cac00a8369510189eb386
Rads <45115368+radsn@users.noreply.github.com>
2024-11-25 18:44:36 +0000

    version 2

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ get '/square/new' do
   erb :square_new
 end
 
-get '/square/results' do
-  number = params[:number].to_i
-  @result = number * number
-  erb :square_results
+get("/square/results") do
+  @sq_number = params.fetch("number").to_f
+  @sq_result = @sq_number ** 2
+  erb(:square_results)
 end
 
 get '/square_root/new' do
@@ -20,8 +20,8 @@ get '/square_root/new' do
 end
 
 get '/square_root/results' do
-  number = params[:number].to_f
-  @result = Math.sqrt(number)
+  @sqr_number = params.fetch("user_number").to_f
+  @sqr_result = Math.sqrt(@sqr_number)
   erb :square_root_results
 end
 
@@ -29,23 +29,25 @@ get '/payment/new' do
   erb :payment_new
 end
 
-get '/payment/results' do
-  apr = params[:user_apr].to_f / 100
-  r = apr / 12 # monthly interest rate
-  n = params[:user_years].to_i * 12 # total number of months
-  pv = params[:user_pv].to_f
-
-  @payment = (r * pv) / (1 - (1 + r)**-n)
-  erb :payment_results
+get("/payment/results") do
+  r = params.fetch("user_apr").to_f / 100 / 12
+  @p_apr = params.fetch("user_apr").to_f.to_fs(:percentage, { :precision => 4 } )
+  @p_years = params.fetch("user_years").to_i
+  n = @p_years * 12
+  pv = params.fetch("user_pv").to_f
+  @p_principal = pv.to_fs(:currency)
+  p = (r * pv) / (1 - ((1 + r) ** (-n)))
+  @p_result = p.to_fs(:currency)
+  erb(:payment_results)
 end
 
 get '/random/new' do
   erb :random_new
 end
 
-get '/random/results' do
-  min = params[:user_min].to_f
-  max = params[:user_max].to_f
-  @random_number = rand(min..max)
-  erb :random_results
-end
+get("/random/results") do
+  @r_min = params.fetch("user_min").to_f
+  @r_max = params.fetch("user_max").to_f
+  @r_result = rand(@r_min..@r_max)
+  erb(:random_results)
+end  
